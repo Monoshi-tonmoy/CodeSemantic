39c44fe596cd61105b94243f36c0967c67b007fd
Monoshi-tonmoy <tonmoy5200@gmail.com>
2025-05-15 23:09:48 -0500

    Added Readme

--- CodeSemantic/Run/RQ1.sh
+++ CodeSemantic/Run/RQ1.sh
@@ -6,10 +6,10 @@ for model_id in 7; do
     echo "Clearing Hugging Face cache before running model_id $model_id..."
     rm -rf $CACHE_PATH
 
-    for shot in 0 1 2 3; do
+    for shot in 0; do
         for incontext in "different"; do
             for CoT in "no"; do
-                for quantized in "yes" "no"; do
+                for quantized in "no" "yes"; do
                     python statement_semantic.py \
                         --data_id 10 \
                         --model_id $model_id \
@@ -25,5 +25,24 @@ for model_id in 7; do
         done
     done
 done
+
+    for shot in 3; do
+        for incontext in "different"; do
+            for CoT in "no"; do
+                for quantized in "no" "yes"; do
+                    python statement_semantic.py \
+                        --data_id 10 \
+                        --model_id $model_id \
+                        --pt_id 1 \
+                        --language python \
+                        --prediction statement \
+                        --shot $shot \
+                        --incontext $incontext \
+                        --CoT $CoT \
+                        --quantized_prediction $quantized
+                done
+            done
+        done
+    done
 
 rm -rf $CACHE_PATH
