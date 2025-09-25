66326247620e8fa6271483a46965ee80f116fb2d
Monoshi-tonmoy <tonmoy5200@gmail.com>
2025-09-24 23:58:39 -0500

    Added the results

--- CodeSemantic/Run/Random_Baseline.sh
+++ CodeSemantic/Run/Random_Baseline.sh
@@ -1,29 +1,19 @@
 #!/bin/bash
 
+CACHE_PATH="/home/monoshi/.cache/huggingface/hub/*"
 cd ..
 
-# for model_id in 20; do
-#     for shot in 3; do
+models=(7 8 9 10 11 12 13 14 15 16 17)
 
-#         python statement_semantic.py \
-#             --data_id 0 \
-#             --model_id $model_id \
-#             --pt_id 1 \
-#             --language python \
-#             --prediction statement \
-#             --shot $shot \
-#             --incontext different \
-#             --CoT no \
-#             --quantized_prediction no \
-#             --quantized_random no \
-#             --API_def no
-#     done
-# done
+for shot in {1..3}; do
+    for model_id in "${models[@]}"; do
+        echo "Clearing Hugging Face cache before running model_id $model_id, shot $shot..."
+        rm -rf $CACHE_PATH
 
-for model_id in 3; do
-    for shot in 0 1 3; do
         if [ "$shot" -eq 0 ]; then
-            for quantized_random in yes no; do
+            for quantized_random in "yes" "no"; do
+                echo "Running shot $shot with quantized_random=$quantized_random..."
+                
                 python statement_semantic.py \
                     --data_id 10 \
                     --model_id $model_id \
@@ -34,10 +24,13 @@ for model_id in 3; do
                     --incontext different \
                     --CoT no \
                     --quantized_prediction yes \
-                    --quantized_random $quantized_random \
-                    --API_def no
+                    --API_def no \
+                    --quantized_random $quantized_random
             done
         else
+            quantized_random="no"
+            echo "Running shot $shot with quantized_random=$quantized_random..."
+            
             python statement_semantic.py \
                 --data_id 10 \
                 --model_id $model_id \
@@ -48,8 +41,11 @@ for model_id in 3; do
                 --incontext different \
                 --CoT no \
                 --quantized_prediction yes \
-                --quantized_random no \
-                --API_def no
+                --API_def no \
+                --quantized_random $quantized_random
         fi
     done
 done
+
+rm -rf $CACHE_PATH
+echo "All experiments completed!"
